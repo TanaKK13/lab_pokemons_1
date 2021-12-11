c2bffc4be5a0b01d08a6a1b50b3aa3afe249ee40
TanaKK13 <90455466+TanaKK13@users.noreply.github.com>
2021-12-11 11:17:54 +0300

    Add files via upload

---
+++ lab1(issues and defend).py
@@ -0,0 +1,53 @@
+import json
+import statistics
+
+file = open('pokemon_full.json')
+pokemons = file.read()
+
+#количество символов в строке
+chr_total = len(pokemons)
+print ('Количество символов в строке:', chr_total)
+
+#количество символов без пробелов и знаков перпинания
+count = 0 
+marks = '!@#$%^&*()_+-!№;%:?*-=,./><{}[]" \''
+count_chars = sum(i in marks for i in pokemons)
+print ('Количество символов в строке без знаков препинания и пробелов:', chr_total - count_chars)
+
+#поиск покемона с максимальной длинной описания
+pokemons_list = json.loads(pokemons)
+res_max_description = 0
+for pokemon in pokemons_list:
+        if pokemon['description'] != "Description not available yet":
+                res_max_description = max(res_max_description, len(pokemon['description']))
+                if res_max_description == len(pokemon['description']):
+                        name_of_pokemon = pokemon['name']
+print ('Имя покемона с максимальной длиной описания:', name_of_pokemon, 'Длина описания:', res_max_description)
+
+#умение с максимальным количеством слов
+res_max_count_words = 0
+for pokemon in pokemons_list:
+        for ability in pokemon ['abilities']:
+                count_words = len(ability.split())
+                res_max_count_words = max(res_max_count_words, count_words)
+                if res_max_count_words == count_words:
+                        max_count_words_of_ability = ability
+                        
+#проверка на несколько умений с максимальным количеством слов
+pokemons_max_abilities = []
+for pokemon in pokemons_list:
+        for ability in pokemon ['abilities']:
+                count_words = len(ability.split())
+                if count_words == res_max_count_words:
+                        pokemons_max_abilities.append (ability)
+if len(pokemons_max_abilities) == 1:
+        print ('Умение, которое содержит больше всего слов:', max_count_words_of_ability)
+else:
+        print ('Умения, которые содержат максималное количество слов:', pokemons_max_abilities)
+
+#защита
+all_type = []
+for pokemon in pokemons_list:
+        for type_pokemon in pokemon ['type']:
+                all_type.append(type_pokemon)
+print('Наиболее часо встречаются покемоныс типом:', statistics.mode(all_type))
